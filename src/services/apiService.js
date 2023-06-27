84ba46464e23c2ee01b3efe208ce14c993402727
Brianclikclak <brianclikclak@hotmail.com>
2023-06-27 10:30:35 +0200

    functions per type and colors added

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,9 +2,26 @@
 
 import axios from 'axios';
 
+const colors = {
+  fire: '#FDDFDF',
+  grass: '#DEFDE0',
+  electric: '#FCF7DE',
+  water: '#DEF3FD',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#98d7a5',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  psychic: '#eaeda1',
+  flying: '#F5F5F5',
+  fighting: '#E6E0D4',
+  normal: '#F5F5F5',
+}; 
+
 
 export async function fetchPokemonList(){
-  const cont =150;
+  const cont = 150;
   const apiUrl="https://pokeapi.co/api/v2/pokemon/";
   const pokemons =[];
   try{
@@ -13,55 +30,30 @@ export async function fetchPokemonList(){
       const pokemon = response.data;
       const pokemonDetails ={
       name: pokemon.name,
-      id: pokemon.id,
+      id: pokemon.id.toString().padStart(3, '0'),
       image: pokemon.sprites.front_default,
-      type: pokemon.types[0].type.name,
+      type: pokemon.types.map((type) => type.type.name).join(', '),
+      colors: pokemon.types.map((type) => colors[type.type.name]),
+      
+      
     };
-    pokemons.push(pokemonDetails);  
+
+  
+  pokemons.push(pokemonDetails);  
+
     console.log(pokemons);
-    }
+    }  
   }  catch(error) {
           console.log("Error: No se pueden obtener los datos", error);
-        };
+        }
   return pokemons;
 }           
 
 
-export function Pokemones(){
-    pokeDetails.value = pokemonData;
-		pokemonImg.value = pokemonData.value.sprites.other.home.front_default;
-		pokemonId.value = pokemonData.value.id;
-		pokemonType.value = pokemonData.value.types[0].type.name;
-		pokeType = pokemonType.value;
 
-    return Pokemones();    
-  }  
   
 
-export function colorPokemon(){
-  
-  const colors = {
-    fire: '#FDDFDF',
-    grass: '#DEFDE0',
-    electric: '#FCF7DE',
-    water: '#DEF3FD',
-    ground: '#f4e7da',
-    rock: '#d5d5d4',
-    fairy: '#fceaff',
-    poison: '#98d7a5',
-    bug: '#f8d5a3',
-    dragon: '#97b3e6',
-    psychic: '#eaeda1',
-    flying: '#F5F5F5',
-    fighting: '#E6E0D4',
-    normal: '#F5F5F5',
-  };
-    const types = pokemon.types.map(type => type.type.name);
-    const type = types[0];
-    return colors[type];
-                       
-  }
-
+   
     
 /*import axios from 'axios'
 
