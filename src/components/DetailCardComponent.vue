ad74512b048a502eeda98794a6db0d044b227591
alexscripts <alejandro.93lf@gmail.com>
2023-06-27 14:26:46 +0200

    few changes

---
+++ src/components/DetailCardComponent.vue
@@ -0,0 +1,33 @@
+<script>
+    import { ref, onMounted } from 'vue';
+    import { PokemonList } from '../services/apiService.js';
+    export default {
+    name: 'DetailCard',
+    setup(){
+        const pokemons = ref([]);
+    
+    onMounted(async() => {
+        pokemons.value= await PokemonList();
+        });
+      
+    return {
+        pokemons,
+    };
+},
+
+    };
+</script>
+
+<template>
+    
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Lato:300,400&display=swap");
+
+
+
+
+</style>
+
+  
